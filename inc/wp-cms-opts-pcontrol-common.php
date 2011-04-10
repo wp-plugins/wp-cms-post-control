08a90a84aa95464c422f298d6a22c2be29a8a692
cmsbuilder <cmsbuilder@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2011-04-10 18:37:18 +0000

    2.5 commit to tags
    git-svn-id: https://plugins.svn.wordpress.org/wp-cms-post-control/tags/2.5@371315 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ inc/wp-cms-opts-pcontrol-common.php
@@ -0,0 +1,5 @@
+		<div id="icon-edit-comments" class="icon32"><br /></div>
+		<h2>More Information</h2>
+		<p><strong>Having problems?</strong> <a href="http://wp-cms.com/our-wordpress-plugins/post-control/" title="Visit the Post Control homepage">Drop by the plugin homepage and leave a comment</a> at <a href="http://wp-cms.com" title="WP-CMS website">http://wp-cms.com</a></p>
+		<p><strong>Love developing themes?</strong> Then check out <a href="http://wonderflux.com" title="Wonderflux theme framework">Wonderflux</a></strong>, the free, advanced theme Framework I've developed!</p>
+		<p><strong>Developed and maintained by:</strong> <a href="http://jonnya.net" title="I make WordPress ZOOM!">Jonnya Creative WordPress Consultant</a>, see a few of the <a href="http://jonnya.net/tools/wordpress/" title="Some of the custom WordPress sites I've built">WordPresss sites I've built</a>.</p>
